033f5e25d4473eea386512330f5b10d51874d403
Alta302 <ckddyd1202@daum.net>
2022-09-14 10:53:45 +0900

    SwiftUI_Project4

---
+++ SwiftUI_Project4/SwiftUI_Project4/SwiftUI_Project4App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Project4App.swift
+//  SwiftUI_Project4
+//
+//  Created by 정창용 on 2022/09/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Project4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
